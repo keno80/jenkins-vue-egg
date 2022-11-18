258cbc123ca5811019fddc77067bec8b3d2461cb
keno80 <1211582428@qq.com>
2022-11-18 11:33:55 +0800

    style: 完善项目页面布局

--- jenkins-front/src/layout/index.vue
+++ jenkins-front/src/layout/index.vue
@@ -1,11 +1,19 @@
 <script lang="ts" setup>
-import {ref} from 'vue'
-import {IconHome, IconCalendar} from '@arco-design/web-vue/es/icon'
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { IconHome, IconCalendar } from '@arco-design/web-vue/es/icon'
+import router from '@/router';
 
+const route = useRoute()
 const collapsed = ref<boolean>(false)
+const defaultSelectedKeys = ref<string[]>([route.fullPath])
 
 const onCollapse = (val: boolean) => {
   collapsed.value = val;
+}
+
+const handleMenuClick = (key: string) => {
+  router.push(key)
 }
 
 </script>
@@ -13,30 +21,22 @@ const onCollapse = (val: boolean) => {
 <template>
   <div class="layout">
     <a-layout class="layout-demo" style="height: 100vh;">
-      <a-layout-sider
-          breakpoint="lg"
-          :width="220"
-          collapsible
-          :collapsed="collapsed"
-          @collapse="onCollapse"
-      >
-        <div class="logo"/>
-        <a-menu
-            :defaultSelectedKeys="['0']"
-        >
-          <a-menu-item key="0">
-            <IconHome/>
-            Menu 1
+      <a-layout-sider breakpoint="lg" :width="220" collapsible :collapsed="collapsed" @collapse="onCollapse">
+        <div class="logo" />
+        <a-menu :defaultSelectedKeys="defaultSelectedKeys" @menu-item-click="handleMenuClick">
+          <a-menu-item key="/">
+            <IconHome />
+            首页
+          </a-menu-item>
+          <a-menu-item key="/project/list">
+            <IconHome />
+            项目列表
           </a-menu-item>
         </a-menu>
       </a-layout-sider>
-      <a-layout style="padding: 0 24px">
-        <a-breadcrumb :style="{ margin: '16px 0' }">
-          <a-breadcrumb-item>Home</a-breadcrumb-item>
-          <a-breadcrumb-item>List</a-breadcrumb-item>
-          <a-breadcrumb-item>App</a-breadcrumb-item>
-        </a-breadcrumb>
-        <a-layout-content>
+      <a-layout>
+        <view class="header">前端自动化部署平台</view>
+        <a-layout-content class="content">
           <RouterView />
         </a-layout-content>
       </a-layout>
@@ -47,5 +47,21 @@ const onCollapse = (val: boolean) => {
 <style lang="scss" scoped>
 .layout {
   height: 100vh;
+
+  .arco-layout {
+    background-color: #f7f7f7;
+
+    .header {
+      padding: 12px 18px;
+      font-size: 18px;
+      background: #fff;
+    }
+
+    .content {
+      background: #fff;
+      margin: 18px;
+      padding: 18px;
+    }
+  }
 }
 </style>
