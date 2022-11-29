26984c80c8a4998c7f1ddf891f09d87a76428ed1
keno80 <1211582428@qq.com>
2022-11-29 13:56:28 +0800

    feat: 服务端添加获取jenkins日志流操作

--- jenkins-server/app/controller/jenkins.js
+++ jenkins-server/app/controller/jenkins.js
@@ -2,7 +2,7 @@
 
 const { Controller } = require('egg');
 const { RESPONSE_CODE } = require('../constant');
-const { buildJob } = require('../../jenkins');
+const { buildJob, waitForBuildNumber, getLogStream } = require('../../jenkins');
 
 class JenkinsController extends Controller {
   async build() {
@@ -10,13 +10,17 @@ class JenkinsController extends Controller {
 
     try {
       const { projectName } = await this.service.config.detail(id);
-      await buildJob(projectName);
+      const buildId = await buildJob(projectName);
+      const buildNumber = await waitForBuildNumber(buildId);
 
       this.ctx.body = {
         code: RESPONSE_CODE.SUCCESS_CODE,
         msg: '构建指令执行成功',
         data: null,
       };
+
+      getLogStream(projectName, buildNumber);
+
     } catch (e) {
       this.ctx.body = {
         code: RESPONSE_CODE.ERROR_CODE,
--- jenkins-server/jenkins/index.js
+++ jenkins-server/jenkins/index.js
@@ -19,7 +19,7 @@ const createJob = async config => {
 
   // console.log(isExist);
 
-  if (!isExist) return jenkins.job.create(config.projectName, jenkinsConfig);
+  if (!isExist) return await jenkins.job.create(config.projectName, jenkinsConfig);
 
   return Promise.reject(new Error('项目已存在'));
   //
@@ -28,16 +28,56 @@ const createJob = async config => {
 
 // 删除jenkins项目
 const destroyJob = async name => {
-  return jenkins.job.destroy(name);
+  return await jenkins.job.destroy(name);
 };
 
 // 构建jenkins项目
 const buildJob = async name => {
-  return jenkins.job.build(name);
+  return await jenkins.job.build(name);
+};
+
+// 获取构建buildNumber
+const waitForBuildNumber = buildId => {
+  return new Promise((resolve, reject) => {
+    const timer = setInterval(async () => {
+      try {
+        const item = await jenkins.queue.item(buildId);
+        if (item.executable) {
+          resolve(item.executable.number);
+          clearInterval(timer);
+        } else if (item.cancelled) {
+          clearInterval(timer);
+          reject;
+        }
+      } catch (err) {
+        reject(err);
+      }
+    }, 1000);
+  });
+};
+
+// 获取jenkins构建日志流
+const getLogStream = (jobName, buildNumber) => {
+  const logStream = jenkins.build.logStream(jobName, buildNumber, 'text', 1000);
+
+  logStream.on('data', text => {
+    console.log(text); // 打印 text 验证日志获取情况
+  });
+
+  logStream.on('error', err => {
+    console.log('error', err);
+  });
+
+  logStream.on('end', () => {
+    console.log('end');
+  });
+
 };
 
 module.exports = {
   createJob,
   destroyJob,
   buildJob,
+  waitForBuildNumber,
+  getLogStream,
 };
